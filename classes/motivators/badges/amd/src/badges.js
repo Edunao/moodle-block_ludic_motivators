15938c9bcf2a22c4a97c8b1c465f59cd6763b309
Didier Gibaud <didier.gibaud@gmail.com>
2018-01-08 18:34:20 +0100

    Added preset array for badges and integration for the badges widget

--- classes/motivators/badges/amd/src/badges.js
+++ classes/motivators/badges/amd/src/badges.js
@@ -1,4 +1,4 @@
-define(['jquery', 'core/tree'], function ($, Tree) {
+/*define(['jquery', 'core/tree'], function ($, Tree) {
     var badges = {
         init: function () {
             console.log('Badges init');
@@ -9,5 +9,112 @@ define(['jquery', 'core/tree'], function ($, Tree) {
             });
         }
     };
+
+    return badges;
+});*/
+
+define(['jquery', 'core/tree'], function ($, Tree) {
+    var badges = {
+        init: function (params) {
+            console.log('Badges SVG init', params);
+
+            var that = this;
+
+            this.params = params;
+
+            this.revealed_pieces = this.params.revealed_pieces;
+
+            this.nb_cols = 4;
+            this.unrevealed_pieces = [];
+            for (var x = 1; x <= this.nb_cols; x++) {
+                for(var y = 1;y <= this.nb_cols; y++) {
+                    if (this.revealed_pieces.indexOf(x+'-'+y) === -1) {
+                        this.unrevealed_pieces.push(x+'-'+y);
+                    }
+                }
+            }
+
+            //convert svg pictures
+            this.convert_svg('img.svg.avatar');
+
+            for (var i in this.revealed_pieces) {
+                that.reveal_piece(this.revealed_pieces[i]);
+            }
+
+            $('#avatar-picture').show();
+
+            $('#next-piece').on('click', function() {
+                that.display_next_piece();
+            });
+        },
+
+        convert_svg : function(selector) {
+            //convert svg pictures
+            $(selector).each(function () {
+                var $img = $(this);
+                var imgID = $img.attr('id');
+                var imgClass = $img.attr('class');
+                var imgURL = $img.attr('src');
+
+                $.get({url : imgURL, async : false}, function (data) {
+                    var $svg = $(data).find('svg');
+                    if (typeof imgID !== 'undefined') {
+                        $svg = $svg.attr('id', imgID);
+                    }
+                    if (typeof imgClass !== 'undefined') {
+                        $svg = $svg.attr('class', imgClass + ' replaced-svg');
+                    }
+                    $svg = $svg.removeAttr('xmlns:a');
+                    $img.replaceWith($svg);
+                });
+            });
+        },
+
+        display_next_piece: function () {
+            var rand = this.get_random_unrevealed_piece();
+
+            if (rand) {
+                this.reveal_piece(rand);
+            }
+            //image complete
+            if (this.unrevealed_pieces.length == 0) {
+                $('#next-piece').hide();
+                $('#congratulation').show();
+            }
+        },
+
+        reveal_piece : function(id) {
+            console.log('reveal piece', id);
+
+            if ($('#piece-'+id).length == 0) {
+                console.log('Piece not found : #piece-' + id);
+            }
+            else {
+                $('#piece-'+id).css({fill : 'transparent', transition: "1.2s"});
+
+                //add piece to revealed pieces
+                var index = this.revealed_pieces.indexOf(id);
+                if (index === -1) {
+                    this.revealed_pieces.push(id);
+                }
+
+                //remove piece from unrevealed pieces
+                var index = this.unrevealed_pieces.indexOf(id);
+                if (index !== -1) {
+                    this.unrevealed_pieces.splice(index, 1);
+                }
+            }
+        },
+
+        get_random_unrevealed_piece : function() {
+            var nb_pieces = this.unrevealed_pieces.length;
+            if (nb_pieces === 0) {
+                return false;
+            }
+            var random_index = Math.floor(Math.random() * nb_pieces);
+            return this.unrevealed_pieces[random_index];
+        }
+    };
     return badges;
 });
+
--- classes/motivators/badges/badges.php
+++ classes/motivators/badges/badges.php
@@ -27,61 +27,135 @@ require_once dirname( __DIR__ ) . '/motivator_interface.php';
 
 class badges extends iMotivator {
 
-	public function __construct($context) {
-		$preset = array(
-			'typeBadge' => 'session',
-			'session' => [
-				'badgeOnSuccessiveAttempt' => 'badge1',
-				'nbquestionOnSuccessiveAttempt' => 5,
-				'badgeAtFirtAttempt' => 'badge2',
-				'badgeAtNAttempt' => 3,
-				'numberattemptAtNAttempt' => 1,
-				'badgeOnSelfCorrection' => 'badge3',
-				'numberattemptOnSelfCorrection' => 1,
-				'courseAchievements' => [
-					"runOfFiveGoodAnswers" => 0,
-					"tenOfTenGoodAnswers" => 1
-				],
-				'globalAchievements' => [
-					'session1Objectives' => 0,
-					'session2Objectives' => 1
-				]
-			],
-			'theme' => [
-				'badgeOnSuccessiveAttempt' => '0',
-				'nbquestionOnSuccessiveAttempt' => '1',
-				'badgeAtFirtAttempt' => '2',
-				'badgeAtNAttempt' => '0',
-				'numberattemptAtNAttempt' => '1',
-				'badgeOnSelfCorrection' => '2',
-				'numberattemptOnSelfCorrection' => '0',
-				'courseAchievements' => [
-					"runOfFiveGoodAnswers" => 0,
-					"tenOfTenGoodAnswers" => 1
-				],
-				'globalAchievements' => [
-					'session1Objectives' => 0,
-					'session2Objectives' => 1
-				]
-			]
-		);
-		parent::__construct($context, $preset);
-	}
+    public function __construct($context) {
+        $preset = array(
+            'coursesGoals' => [
+                [
+                    'badgeName' => '3 bonnes réponses',
+                    'iconId' => 'course_badge1',
+                    'achievement' => $this::BLOCK_LUDICMOTIVATORS_STATE_PREVIOUSLYACHIEVED,
+                ],
+                [
+                    'badgeName' => 'Course Goal 2',
+                    'iconId' => 'course_badge2',
+                    'achievement' => $this::BLOCK_LUDICMOTIVATORS_STATE_NOTACHIEVED,
+                ],
+                [
+                    'badgeName' => '10 bonnes réponses',
+                    'iconId' => 'course_badge2',
+                    'achievement' => $this::BLOCK_LUDICMOTIVATORS_STATE_PREVIOUSLYACHIEVED,
+                ],
+                [
+                    'badgeName' => 'Course Goal 4',
+                    'iconId' => 'course_badge1',
+                    'achievement' => $this::BLOCK_LUDICMOTIVATORS_STATE_NOTACHIEVED,
+                ],
+                [
+                    'badgeName' => 'Course Goal 5',
+                    'iconId' => 'course_badge1',
+                    'achievement' => $this::BLOCK_LUDICMOTIVATORS_STATE_JUSTACHIEVED,
+                ]
+            ],
+            'globalsGoals' => [
+                [
+                    'layerName' => '1-1',
+                    'achievement' => $this::BLOCK_LUDICMOTIVATORS_STATE_PREVIOUSLYACHIEVED,
+                ],
+                [
+                    'layerName' => '1-4',
+                    'achievement' => $this::BLOCK_LUDICMOTIVATORS_STATE_PREVIOUSLYACHIEVED,
+                ],
+                [
+                    'layerName' => '2-2',
+                    'achievement' => $this::BLOCK_LUDICMOTIVATORS_STATE_PREVIOUSLYACHIEVED,
+                ]
+            ]
+        );
+        parent::__construct($context, $preset);
+    }
 
-	public function getTitle() {
+    public function getTitle() {
 
-		return 'Mes badges';
-	}
+        return 'Mes badges';
+    }
 
-	public function get_content() {
-		global $CFG;
+    public function get_content() {
+        global $CFG;
 
-		$output = '<div id="badges-container">';
-		$output .= '<div class="ludic_motivators-badge"><img src="'.$CFG->wwwroot.'/blocks/ludic_motivators/classes/motivators/badges/pix/badge1.png" title="3 bonnes réponses"/></div>';
-		$output .= '<div class="ludic_motivators-badge"><img src="'.$CFG->wwwroot.'/blocks/ludic_motivators/classes/motivators/badges/pix/badge2.png" title="10 bonnes réponses" style="display:none;"/></div>';
-		$output .= '</div>';
+        $output  = '<div id="badges-container">';
 
-		return $output;
-	}
+        // Div block for course goals selected between the achieved and not achieved (opacity:0.3)
+        $output .=
+            '<div>
+                <h4 style="background-color: #6F5499;color: #CDBFE3;text-align: center;">Course Goals achieved and not</h4>
+                <ul id="course-badge">';
+        foreach ($this->preset['coursesGoals'] as $key => $courseBadge) {
+            $opacity = ($courseBadge['achievement'] === $this::BLOCK_LUDICMOTIVATORS_STATE_NOTACHIEVED) ? "0.3" : "1";
+            $output .=
+                '<li style="opacity:' . $opacity . '">
+                    <figure>
+                        <img src="' . $CFG->wwwroot . '/blocks/ludic_motivators/classes/motivators/badges/pix/' . $courseBadge['iconId'] . '.png " title="' . $courseBadge['badgeName'] . '"/>
+                        <figcaption>' . $courseBadge['badgeName'] . '</figcaption>
+                    </figure>
+                </li>';
+        }
+        $output .=
+            '   </ul>
+            </div>';
+
+        // Div block for course goals that have just been achieved
+        $output .=
+            '<div>
+                <h4 style="background-color: #6F5499;color: #CDBFE3;text-align: center;">Course Goals just achieved</h4>
+                <ul id="course-badge">';
+        foreach ($this->preset['coursesGoals'] as $key => $courseBadge) {
+            if ($courseBadge['achievement'] === $this::BLOCK_LUDICMOTIVATORS_STATE_JUSTACHIEVED) {
+                $output .=
+                '<li>
+                    <figure>
+                        <img src="' . $CFG->wwwroot . '/blocks/ludic_motivators/classes/motivators/badges/pix/' . $courseBadge['iconId'] . '.png " title="' . $courseBadge['badgeName'] . '"/>
+                        <figcaption>' . $courseBadge['badgeName'] . '</figcaption>
+                    </figure>
+                </li>';
+            }
+        }
+        $output .=
+            '   </ul>
+            </div>';
+
+        // Div block displaying an SVG image representing the global goals with layers that
+        // can be displayed to represent the goals that have been achieved
+        $output .=
+            '<div id="avatar-container">
+                <h4 style="background-color: #6F5499;color: #CDBFE3;text-align: center;">Global Goals</h4>';
+        $output .=
+                '<div /*style="position:relative"*/>
+                    <img src="'.$this->image_url('puzzle.svg').'" width="180px" height="180px" class="avatar svg"/>
+                </div>';
+        $output .=
+            '</div>';
+
+        return $output;
+    }
+
+
+    public function getJsParams() {
+        $datas = $this->context->store->get_datas();
+        $params = array('revealed_pieces' => array());print_r($this->preset['globalsGoals']);
+
+        foreach ($this->preset['globalsGoals'] as $key => $value) {
+            if ($value['achievement'] == $this::BLOCK_LUDICMOTIVATORS_STATE_PREVIOUSLYACHIEVED) {
+                $params['revealed_pieces'][] = $value['layerName'];
+            }
+        }
+
+        if (isset($datas->avatar)) {
+            $params = $datas->avatar;
+        }
+
+        print_r($params['revealed_pieces']);
+
+        return $params;
+    }
 
 }
