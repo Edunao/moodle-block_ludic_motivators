2d872971917fc804a70d850541f12a38d476e1ec
Didier Gibaud <didier.gibaud@gmail.com>
2018-01-09 15:46:59 +0100

    Added preset array and branch and tree puzzle SVG for progress motivator

--- classes/motivators/progress/progress.php
+++ classes/motivators/progress/progress.php
@@ -27,34 +27,165 @@ require_once dirname( __DIR__ ) . '/motivator_interface.php';
 
 class progress extends iMotivator {
 
-	public function __construct($context) {
-		$preset = array(
-			'introductionMessage' => 'Bravo, tu as réussi, maintenant avec un chrono, essaie de faire de ton mieux',
-			'maxDurationTimer' => 90,
-			'numberAttempts' => 3,
-			'attemptsTiming' => [40, 60, 30],
-			'courseAchievements' => [
-				"runOfFiveGoodAnswers" => 0,
-				"tenOfTenGoodAnswers" => 1
-			],
-			'globalAchievements' => [
-				'session1Objectives' => 0,
-				'session2Objectives' => 1
-			]
-		);
-		parent::__construct($context, $preset);
-	}
+    public function __construct($context) {
+        $preset = array(
+            'branches' => [
+                [
+                    'courseId' => '1',
+                    'achievement' => $this::BLOCK_LUDICMOTIVATORS_STATE_PREVIOUSLYACHIEVED,
+                    'layers' => [
+                        [
+                            'layerName' => '1-1',
+                            'achievement' => $this::BLOCK_LUDICMOTIVATORS_STATE_PREVIOUSLYACHIEVED,
+                        ],
+                        [
+                            'layerName' => '2-1',
+                            'achievement' => $this::BLOCK_LUDICMOTIVATORS_STATE_NOTACHIEVED,
+                        ],
+                    ]
+                ],
+                [
+                    'courseId' => '2',
+                    'achievement' => $this::BLOCK_LUDICMOTIVATORS_STATE_PREVIOUSLYACHIEVED,
+                    'layers' => [
+                        [
+                            'layerName' => '2-2',
+                            'achievement' => $this::BLOCK_LUDICMOTIVATORS_STATE_PREVIOUSLYACHIEVED,
+                        ],
+                        [
+                            'layerName' => '4-2',
+                            'achievement' => $this::BLOCK_LUDICMOTIVATORS_STATE_PREVIOUSLYACHIEVED,
+                        ],
+                    ]
+                ],
+                [
+                    'courseId' => '3',
+                    'achievement' => $this::BLOCK_LUDICMOTIVATORS_STATE_PREVIOUSLYACHIEVED,
+                    'layers' => [
+                        [
+                            'layerName' => '1-3',
+                            'achievement' => $this::BLOCK_LUDICMOTIVATORS_STATE_PREVIOUSLYACHIEVED,
+                        ],
+                    ]
+                ],
+                [
+                    'courseId' => '4',
+                    'achievement' => $this::BLOCK_LUDICMOTIVATORS_STATE_PREVIOUSLYACHIEVED,
+                    'layers' => [
+                        [
+                            'layerName' => '2-4',
+                            'achievement' => $this::BLOCK_LUDICMOTIVATORS_STATE_PREVIOUSLYACHIEVED,
+                        ],
+                        [
+                            'layerName' => '3-4',
+                            'achievement' => $this::BLOCK_LUDICMOTIVATORS_STATE_PREVIOUSLYACHIEVED,
+                        ],
+                        [
+                            'layerName' => '4-4',
+                            'achievement' => $this::BLOCK_LUDICMOTIVATORS_STATE_PREVIOUSLYACHIEVED,
+                        ],                    ]
+                ],
+            ]
+        );
+        parent::__construct($context, $preset);
+    }
 
-	public function getTitle() {
+    public function getTitle() {
 
-		return 'My progress';
-	}
+        return 'My progress';
+    }
 
-	public function get_content() {
-		$output = '<div id="progress-container">';
-		$output .= '<div class="progress"/><span class="progress-number">136</span><span class="points">pts</span></div>';
-		$output .= '</div>';
+    /*
+     * This determines whether or not the current page is /my/ page
+     *
+     * @return true or false
+     */
+    function isMyPage() {
+        $arrayPathUrl = explode('/', $_SERVER['REQUEST_URI']);
+        if ($arrayPathUrl[count($arrayPathUrl) - 2] === 'my') {
+            return true;
+        }
 
-		return $output;
-	}
+        return false;
+    }
+
+    /*
+     * This return an array containing the layer name of the goals previously achieved
+     *
+     * @return an array
+     */
+    function getRevealedPieces(array $layers){
+        //$datas = $this->context->store->get_datas();
+        //$params = array('revealed_pieces' => array());
+
+        foreach ($layers as $key => $layer) {
+            if ($layer['achievement'] == $this::BLOCK_LUDICMOTIVATORS_STATE_PREVIOUSLYACHIEVED) {
+                $revealedPieces[] = $layer['layerName'];
+            }
+        }
+
+        return $revealedPieces;
+    }
+
+    public function get_content() {
+        // The view for the /my/ page shows an image of trees with 8 optional layers per course
+        // (making 14 x 8 = 112 optional layers in all)
+        if ($this->isMyPage()) {
+            $output  = '<div>
+                            <h4 style="background-color: #6F5499;color: #CDBFE3;text-align: center;">Tree</h4>
+                            <div id="progress-container">';
+            $output .= '        <img src="'.$this->image_url('tree.svg').'" width="180px" height="180px" class="avatar svg"/>';
+            $output .= '    </div>
+                        </div>';
+            $output .= '<div><button id="next-piece">Répondre à une question</button></div>';
+            $output .= '<div id="congratulation">Congratulations !</div>';
+
+        // The view within a course shows a tree branch as an SVG with 8 optional layers.
+        // The progress value (0..8) determines which of the layers will be hidden and which revealed
+        } else {
+            $output  = '<div>
+                            <h4 style="background-color: #6F5499;color: #CDBFE3;text-align: center;">Branch</h4>
+                            <div id="progress-container">';
+            $output .= '        <img src="'.$this->image_url('branch.svg').'" width="180px" height="180px" class="avatar svg"/>';
+            $output .= '    </div>
+                        </div>';
+            $output .= '<div><button id="next-piece">Répondre à une question</button></div>';
+            $output .= '<div id="congratulation">Congratulations !</div>';
+        }
+
+        return $output;
+    }
+
+    public function getJsParams() {
+        $datas = $this->context->store->get_datas();
+        $params = array('revealed_pieces' => array());
+
+        // The view for the /my/ page shows an image of trees with 8 optional layers per course
+        // (making 14 x 8 = 112 optional layers in all)
+        if ($this->isMyPage()) {
+            foreach ($this->preset['branches'] as $key => $branch) {
+                //if ($branch['achievement'] == $this::BLOCK_LUDICMOTIVATORS_STATE_PREVIOUSLYACHIEVED) {
+                $params['revealed_pieces'] = array_merge(
+                    $params['revealed_pieces'],
+                    $this->getRevealedPieces($branch['layers'])
+                );
+                //}
+            }
+
+        // The view within a course shows a tree branch as an SVG with 8 optional layers.
+        // The progress value (0..8) determines which of the layers will be hidden and which revealed
+        } else {
+            foreach ($this->preset['branches'] as $key => $branch) {
+                if ($branch['courseId'] === $this->context->getCourseId()) {
+                    $params['revealed_pieces'] = $this->getRevealedPieces($branch['layers']);
+                }
+            }
+        }
+
+        if (isset($datas->avatar)) {
+            $params = $datas->avatar;
+        }
+
+        return $params;
+    }
 }
